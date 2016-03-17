b2e7570b8f4639cede1b452f8e032acb432413f7
SuperNova <hwchon1234@naver.com>
2016-03-17 20:55:10 +0900

    Iterating Over Data With JSX - Chapter 12

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactsList from './ContactsList';
 
-let contacts = {
+let contacts = [{
+  id: 1,
   name: 'Scott',
   phone: '555 555 5555'
-};
+}, {
+  id: 2,
+  name: 'Courtney',
+  phone: '555 111 5555'
+}, {
+  id: 3,
+  name: 'Tim',
+  phone: '555 222 5555'
+}, {
+  id: 4,
+  name: 'Jeff',
+  phone: '555 333 5555'
+}];
 
 class App extends React.Component {
   render () {
