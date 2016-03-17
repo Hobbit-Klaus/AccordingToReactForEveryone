e38d852586f9bb25fb0143e9b3c5ac1de94b897e
SuperNova <hwchon1234@naver.com>
2016-03-17 18:31:06 +0900

    React.render() To ReactDOM.render()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
   render () {
@@ -10,4 +11,4 @@ class App extends React.Component {
   }
 }
 
-React.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
